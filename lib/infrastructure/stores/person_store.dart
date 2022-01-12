76690dcd13f8bb13c2428d7736c097f7c2b82c1a
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2022-01-12 05:52:44 -0300

    Alteração no nome da classe Person

--- lib/infrastructure/stores/person_store.dart
+++ lib/infrastructure/stores/person_store.dart
@@ -1,27 +1,31 @@
 import 'package:get/get.dart';
 import 'package:http/http.dart';
-import 'package:teste_unitarios/domain/entities/person.dart';
+import 'package:teste_unitarios/domain/entities/person_entity.dart';
 import 'package:teste_unitarios/infrastructure/repositories/person_repository.dart';
 
 class PersonStore extends GetxController {
   final _isLoading = false.obs;
 
-  final RxList<Person> _listaPessoas = RxList<Person>();
+  final RxList<PersonEntity> _listaPessoas = RxList<PersonEntity>();
 
   get isLoading => _isLoading.value;
   set isLoading(value) => _isLoading.value = value;
 
-  List<Person> get listaPessoas => _listaPessoas;
+  List<PersonEntity> get listaPessoas => _listaPessoas;
 
   listarPessoas() async {
     isLoading = true;
     try {
-      var lista = await PersonRepository(Client()).getPerson();
+      var lista = await PersonRepository(Client()).getListOfPerson();
       _listaPessoas.clear();
       // await Future.delayed(Duration(seconds: 10));
       _listaPessoas.addAll(lista);
     } finally {
       isLoading = false;
     }
+  }
+
+  PersonEntity buscarPessoa() {
+    return PersonEntity('1', 'name', 0, 0, 0);
   }
 }
